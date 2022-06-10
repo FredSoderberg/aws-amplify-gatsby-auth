b7e07aa413733006365df4528fe3e7fc0fac18ff
Fredrik Söderberg <fredriksoderberg@Fredrik.local>
2022-06-10 15:38:06 +0200

    test 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import Amplify from 'aws-amplify';
+import { Link } from 'gatsby';
+import React from 'react';
+import config from '../aws-exports';
+import Layout from '../components/layout';
 
-import Layout from '../components/layout'
 
-import Amplify from 'aws-amplify'
-import config from '../aws-exports'
 Amplify.configure(config)
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
+    <h2>Peeps!</h2>
     <p>Welcome to your new Gatsby site with multi-user authentication powered by <a href="https://amplify.aws">AWS Amplify</a></p>
     <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>
     <Link to="/app/login">Sign In</Link><br />
